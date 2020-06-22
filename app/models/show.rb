4141d135a5bfd05b0e716523eeafa68618c0cdd9
Bryce B <bryce.boddy@gmail.com>
2020-06-22 08:20:33 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,29 @@
+class Show < ActiveRecord::Base
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+  
+  def self.most_popular_show
+    self.where("rating = ?", self.highest_rating).first
+  end
+  
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+  
+  def self.least_popular_show
+    self.where("rating = ?", self.lowest_rating).first
+  end
+  
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+  
+  def popular_shows
+    self.where("rating > 5")
+  end
+  
+  def shows_by_alphabetical_order
+    Movie.order(:name)
+  end
+end
